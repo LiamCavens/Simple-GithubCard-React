078fa07e2a13678a570278ca1c3257df50418948
Liam Cavens <liamcavens@googlemail.com>
2018-08-08 14:06:28 +0100

    worked on card component

---
+++ src/component/Card.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Card extends Component {
+render() {
+return (
+<div>
+    <img src="http://placehold.it/75"/>
+    <div>
+        <div>Name here...</div>
+        <div>Company name here...</div>
+    </div>
+</div>
+);
+}
+}
