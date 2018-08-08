487ddcb884decac4b31fbd07e6aa6a23e3f00d72
Liam Cavens <liamcavens@googlemail.com>
2018-08-08 14:33:05 +0100

    added form component

---
+++ src/component/Form.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class extends Component {
+  render() {
+    return 
+        <form>
+            <input type="text" placeholder="Github username" />
+            <button type="submit">Add Card</button>
+        </form>
+    ;
+  }
+}
